d717ea83731b49c4a7416702002eecbd77e35522
TravisWongX <travis.x.wong@gmail.com>
2021-08-07 17:51:14 +0800

    finish

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
-logger.info('connecting to', config.MONGODB_URI)
+const url = config.MONGODB_URI
+logger.info('connecting to', url)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
+    logger.info('error connecting to MongoDB:', error.message)
   })
 
+morgan.token('body', function (req) { return JSON.stringify(req.body) })
+
+app.use(bodyParser.json())
 app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
 
-app.use(middleware.unknownEndpoint)
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
+app.use(middleware.tokenExtractor)
+app.use(middleware.tokenValidator)
+
+app.use('/api/blogs', blogRouter)
+
 app.use(middleware.errorHandler)
 
 module.exports = app
